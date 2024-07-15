895ba4f826e3d2ece5b9b6d4a7ba4c938d05c404
Brandon Chao <iDarky87@gmail.com>
2024-07-15 19:06:35 -0400

    learned about jinja2, https://www.geeksforgeeks.org/templating-with-jinja2-in-flask/, made the basic layout of the website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect, url_for
 import requests
 
 app = Flask(__name__)
@@ -7,13 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
 @app.route('/quiz/<level>')
 def quiz(level):
     return render_template('quiz.html', level=level)
 
-@app.route('/api/stock_data')
-def stock_data():
-    response = requests.get("") # url goes here
+@app.route('/api/stock_data/ticker')
+def stock_data(ticker):
+    response = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{ticker}/prev?apiKey={vAycONtalDFVJ6kECb9UcK7FBD7MWwpY}") # url goes here
     data = response.json()
     return jsonify(data)
 
